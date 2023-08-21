186b04114471d436a21d56c0130774dccc122be8
Frieda Haringsma <fharingsma@hotmail.com>
2023-08-21 11:30:59 +0200

    url hardcoded works

--- script/script.js
+++ script/script.js
@@ -1,165 +1,162 @@
-// Retrieve categories from local storage
-const category1 = localStorage.getItem('category1') || 'Category 1';
-const category2 = localStorage.getItem('category2') || 'Category 2';
-const category3 = localStorage.getItem('category3') || 'Category 3';
-const category4 = localStorage.getItem('category4') || 'Category 4';
-const saveCategories = document.getElementById('save-categories');
+window.onload = () => {
+  // Retrieve categories from local storage
+  const category1 = localStorage.getItem('category1') || 'Category 1';
+  const category2 = localStorage.getItem('category2') || 'Category 2';
+  const category3 = localStorage.getItem('category3') || 'Category 3';
+  const category4 = localStorage.getItem('category4') || 'Category 4';
 
-// refresh page to save new input categories
-saveCategories.addEventListener('click', () => {
-  location.reload();
-});
+  const saveCategories = document.getElementById('save-categories');
+  const inputDescription = document.getElementById('input-description');
+  const saveBtn = document.getElementById('save-btn');
+  const getLeads = document.getElementById('get-leads');
 
-// Get DOM elements for the buttons
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
-const button4 = document.getElementById('button4');
-
-// Create category element
-const categoryElement = document.createElement('p');
-categoryElement.id = 'category';
-
-// Set the active category initially
-let activeCategory = category1;
-
-// Load category names into buttons
-button1.innerHTML = category1;
-button2.innerHTML = category2;
-button3.innerHTML = category3;
-button4.innerHTML = category4;
-
-// Edit buttons
-button1.addEventListener('click', () => {
-  enableButtonEdit(button1, 'category1');
-  setActiveCategory(category1);
-  renderLeads();
-});
-
-button2.addEventListener('click', () => {
-  enableButtonEdit(button2, 'category2');
-  setActiveCategory(category2);
-  renderLeads();
-});
-
-button3.addEventListener('click', () => {
-  enableButtonEdit(button3, 'category3');
-  setActiveCategory(category3);
-  renderLeads();
-});
-
-button4.addEventListener('click', () => {
-  enableButtonEdit(button4, 'category4');
-  setActiveCategory(category4);
-  renderLeads();
-});
-
-// Set active category
-const setActiveCategory = (category) => {
-  activeCategory = category;
-  categoryElement.textContent = activeCategory;
-};
-
-// Enable button edit
-const enableButtonEdit = (button, storageKey) => {
-  button.contentEditable = true;
-
-  button.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      button.blur();
-    }
+  // refresh page to save new input categories
+  saveCategories.addEventListener('click', () => {
+    location.reload();
   });
 
-  button.addEventListener('blur', function () {
-    button.contentEditable = false;
-    localStorage.setItem(storageKey, button.innerHTML);
-  });
-};
+  // Create category element
+  const categoryElement = document.createElement('p');
+  categoryElement.id = 'category';
 
-// Getting the leads for display
-let myLeads = [];
+  // Get DOM elements for the buttons
+  const button1 = document.getElementById('button1');
+  const button2 = document.getElementById('button2');
+  const button3 = document.getElementById('button3');
+  const button4 = document.getElementById('button4');
 
-// Get DOM elements for the leads
-const inputDescription = document.getElementById('input-description');
-const saveBtn = document.getElementById('save-btn');
-const getLeads = document.getElementById('get-leads');
+  // Set the active category initially
+  let activeCategory = category1;
 
-// get tabs for urls
-const tabs = [{ url: 'https://www.xaci-development.com' }];
+  // Load category names into buttons
+  button1.innerHTML = category1;
+  button2.innerHTML = category2;
+  button3.innerHTML = category3;
+  button4.innerHTML = category4;
 
-// Save button click event
-saveBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  myLeads.push(inputDescription.value);
-  myLeads.push(tabs[0].url);
-  localStorage.setItem(activeCategory, JSON.stringify(myLeads));
-  renderLeads();
-});
-
-// Render leads
-const renderLeads = () => {
-  getLeads.innerHTML = '';
-  myLeads = JSON.parse(localStorage.getItem(activeCategory)) || [];
-
-  const title = document.createElement('h2');
-  title.textContent = activeCategory;
-  getLeads.appendChild(title);
-
-  for (let i = 0; i < myLeads.length; i++) {
-    const leadsContainer = document.createElement('div');
-    leadsContainer.className = 'leads-container';
-
-    const text = document.createElement('p');
-    text.textContent = myLeads[i];
-
-    const category = document.createElement('p');
-    category.textContent = activeCategory;
-
-    const url = document.createElement('a');
-    url.href = tabs[0].url;
-    url.textContent = tabs[0].url;
-    url.target = '_blank';
-
-    const deleteItem = document.createElement('button');
-    deleteItem.className = 'delete-item-btn';
-    const deleteIcon = document.createElement('span');
-    deleteIcon.className = 'material-symbols-outlined';
-    deleteIcon.textContent = 'delete';
-    deleteItem.appendChild(deleteIcon);
-
-    // Add event listener to delete the specific item
-    deleteItem.addEventListener('click', () => {
-      deleteSpecificItem(i);
-      renderLeads();
-    });
-
-    leadsContainer.appendChild(text);
-    leadsContainer.appendChild(category);
-    leadsContainer.appendChild(deleteItem);
-    leadsContainer.appendChild(url);
-
-    getLeads.appendChild(leadsContainer);
-  }
-
-  const deleteAll = document.createElement('button');
-  deleteAll.textContent = 'Delete all';
-  // Delete all items
-  deleteAll.addEventListener('click', () => {
-    localStorage.removeItem(activeCategory);
+  // Edit buttons
+  button1.addEventListener('click', () => {
+    enableButtonEdit(button1, 'category1');
+    setActiveCategory(category1);
     renderLeads();
   });
 
-  getLeads.appendChild(deleteAll);
-};
+  button2.addEventListener('click', () => {
+    enableButtonEdit(button2, 'category2');
+    setActiveCategory(category2);
+    renderLeads();
+  });
 
-// Delete a specific item from the leads array
-const deleteSpecificItem = (index) => {
-  const updatedLeads = JSON.parse(localStorage.getItem(activeCategory)) || [];
+  button3.addEventListener('click', () => {
+    enableButtonEdit(button3, 'category3');
+    setActiveCategory(category3);
+    renderLeads();
+  });
 
-  if (index >= 0 && index < updatedLeads.length) {
-    updatedLeads.splice(index, 1);
-  }
+  button4.addEventListener('click', () => {
+    enableButtonEdit(button4, 'category4');
+    setActiveCategory(category4);
+    renderLeads();
+  });
 
-  localStorage.setItem(activeCategory, JSON.stringify(updatedLeads));
+  // Set active category
+  const setActiveCategory = (category) => {
+    activeCategory = category;
+    categoryElement.textContent = activeCategory;
+  };
+
+  // Enable button edit
+  const enableButtonEdit = (button, storageKey) => {
+    button.contentEditable = true;
+
+    button.addEventListener('keydown', function (event) {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        button.blur();
+      }
+    });
+
+    button.addEventListener('blur', function () {
+      button.contentEditable = false;
+      localStorage.setItem(storageKey, button.innerHTML);
+    });
+  };
+
+  // Getting the leads for display
+  let myLeads = [];
+
+  // Save button click event
+  saveBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const tabs = [{ url: 'https://www.xaci-development.com' }];
+    myLeads.push(inputDescription.value);
+    myLeads.push(tabs[0].url);
+    localStorage.setItem(activeCategory, JSON.stringify(myLeads));
+    renderLeads();
+  });
+
+  // Render leads
+  const renderLeads = () => {
+    getLeads.innerHTML = '';
+    myLeads = JSON.parse(localStorage.getItem(activeCategory)) || [];
+
+    const title = document.createElement('h2');
+    title.textContent = activeCategory;
+    getLeads.appendChild(title);
+
+    for (let i = 0; i < myLeads.length; i += 2) {
+      const leadsContainer = document.createElement('div');
+      leadsContainer.className = 'leads-container';
+
+      const text = document.createElement('p');
+      text.textContent = myLeads[i];
+
+      const category = document.createElement('p');
+      category.textContent = activeCategory;
+
+      const url = document.createElement('a');
+      url.href = myLeads[i + 1];
+      url.textContent = myLeads[i + 1];
+      url.target = '_blank';
+
+      const deleteItem = document.createElement('button');
+      deleteItem.className = 'delete-item-btn';
+      const deleteIcon = document.createElement('span');
+      deleteIcon.className = 'material-symbols-outlined';
+      deleteIcon.textContent = 'delete';
+      deleteItem.appendChild(deleteIcon);
+
+      deleteItem.addEventListener('click', () => {
+        deleteSpecificItem(i);
+        renderLeads();
+      });
+
+      leadsContainer.appendChild(text);
+      leadsContainer.appendChild(category);
+      leadsContainer.appendChild(deleteItem);
+      leadsContainer.appendChild(url);
+
+      getLeads.appendChild(leadsContainer);
+    }
+    const deleteAll = document.createElement('button');
+    deleteAll.textContent = 'Delete all';
+    // Delete all items
+    deleteAll.addEventListener('click', () => {
+      localStorage.removeItem(activeCategory);
+      renderLeads();
+    });
+
+    getLeads.appendChild(deleteAll);
+  };
+
+  // Delete a specific item from the leads array
+  const deleteSpecificItem = (index) => {
+    const updatedLeads = JSON.parse(localStorage.getItem(activeCategory)) || [];
+
+    if (index >= 0 && index < updatedLeads.length) {
+      updatedLeads.splice(index, 2);
+    }
+
+    localStorage.setItem(activeCategory, JSON.stringify(updatedLeads));
+  };
 };
